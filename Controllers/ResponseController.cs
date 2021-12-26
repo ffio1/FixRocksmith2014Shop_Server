c25fdad3fe369f032918b5e08bfcb24a4dfda4a9
ffio1 <ffio1@users.noreply.github.com>
2021-12-26 15:58:28 -0800

    Fix Project import

---
+++ Controllers/ResponseController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+#pragma warning disable CS8601 // Possible null reference assignment.
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+
+namespace Proxy.Controllers
+{
+    [Route("api/requests/")]
+    [ApiController]
+    public class ResponseController : Controller
+    {
+        int? lastPackageId = 0;
+        string currency = "USD"; // Show USD just in-case the first song in the store has been delisted.
+        static Dictionary<int, JsonResult> Cache = new();
+
+        [HttpGet("{AppId},{CountryCode},{Language}")]
+        public async Task<JsonResult> GetResponse(int AppId, string CountryCode, string Language)
+        {
+            using (HttpClient http = new HttpClient())
+            {
+                if (Cache.ContainsKey(AppId)) // We've already cached this result.
+                    return Cache[AppId];
+
+                ResponseContainer? steamResponse = await http.GetFromJsonAsync<ResponseContainer>($"http://api.steampowered.com/service/Store/GetAppInfo/?input_json={{%22appids%22:[{AppId}],%22country_code%22:%22{CountryCode}%22,%22language%22:%22{Language}%22,%22query_app_title%22:true,%22query_package_price%22:true,%22query_app_description%22:true}}");
+
+                if (steamResponse == null || steamResponse.Response == null || steamResponse.Response.Apps == null || steamResponse.Response?.Apps?.Length == 0)
+                    return Json("response:{}");
+
+                if (steamResponse.Response?.Apps[0].OriginalPrice == null) // Song has been delisted, and we need to fake the data so the shop doesn't break.
+                {
+                    steamResponse.Response.Apps[0].OriginalPrice = 0;
+                    steamResponse.Response.Apps[0].CurrentPrice = 0;
+                    steamResponse.Response.Apps[0].PackageId = lastPackageId;
+                    steamResponse.Response.Apps[0].DiscountPercent = 0;
+                    steamResponse.Response.Apps[0].Currency = currency;
+                    steamResponse.Response.Apps[0].Description = "DELISTED! Unable to purchase. " + steamResponse.Response.Apps[0].Description;
+                }
+                else
+                {
+                    if (currency != steamResponse.Response.Apps[0].Currency) // If the users currency isn't the same as we have saved, replace the one we have saved.
+                        currency = steamResponse.Response.Apps[0].Currency;
+
+                    if (lastPackageId != steamResponse.Response.Apps[0].PackageId) // Save a real packageid so if we have a delisted song, we can use that to fake our way through parsing.
+                        lastPackageId = steamResponse.Response.Apps[0].PackageId;
+                }
+
+                Debug.WriteLine($"Sent response for {AppId} - {steamResponse.Response.Apps[0].Title}");
+
+                JsonResult result = Json(steamResponse);
+
+                if (!Cache.ContainsKey(AppId)) 
+                {
+                    Cache.Add(AppId, result); // Cache the json response so we don't have to remake this.
+                }
+
+                return result;
+            }
+        }
+    }
+}
---
+++ Models/Response.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Proxy
+{
+    [Serializable]
+    public class ResponseContainer
+    {
+        [JsonPropertyName("response")]
+        public AppsContainer? Response { get; set; }
+    }
+
+    [Serializable]
+    public class AppsContainer
+    {
+        [JsonPropertyName("apps")]
+        public Response[]? Apps { get; set; }
+    }
+
+    [Serializable]
+    public class Response
+    {
+        [JsonPropertyName("appid")]
+        public int AppId { get; set; }
+
+        [JsonPropertyName("app_title")]
+        public string? Title { get; set; }
+
+        [JsonPropertyName("packageid")]
+        public int? PackageId { get; set; }
+
+        [JsonPropertyName("package_currency")]
+        public string? Currency { get; set; }
+
+        [JsonPropertyName("package_initial_price")]
+        public int? OriginalPrice { get; set; }
+
+        [JsonPropertyName("package_final_price")]
+        public int? CurrentPrice { get; set; }
+
+        [JsonPropertyName("package_discount_percent")]
+        public int? DiscountPercent { get; set; }
+
+        [JsonPropertyName("app_description")]
+        public string? Description { get; set; }
+    }
+}
