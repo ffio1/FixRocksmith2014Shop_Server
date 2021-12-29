8dd529c9695594dd89fd802901cf63928ba73c92
ffio1 <ffio1@users.noreply.github.com>
2021-12-28 16:21:13 -0800

    Delisted = Don't show in shop & Remove currency caching

--- Controllers/ResponseController.cs
+++ Controllers/ResponseController.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
-#pragma warning disable CS8604 // Possible null reference argument.
 
 namespace Proxy.Controllers
 {
@@ -10,16 +9,18 @@ namespace Proxy.Controllers
     [ApiController]
     public class ResponseController : Controller
     {
-        static Random random = new();
         static Dictionary<string, Dictionary<int, JsonResult>> Cache = new();
 
-        static Dictionary<string, string> CountryCodeToCurrency = new();
+        static JsonResult? GenericError = null;
 
         [HttpGet("{AppId},{CountryCode},{Language}.{BypassCache?}")]
         public async Task<JsonResult> GetResponse(int AppId, string CountryCode, string Language, bool? BypassCache = null)
         {
             using (HttpClient http = new())
             {
+                if (GenericError == null)
+                    GenericError = Json("response:{}");
+
                 if (BypassCache == null || BypassCache == false)
                 {
                     if (Cache.ContainsKey(CountryCode))
@@ -34,26 +35,16 @@ namespace Proxy.Controllers
                 ResponseContainer? steamResponse = await http.GetFromJsonAsync<ResponseContainer>($"http://api.steampowered.com/service/Store/GetAppInfo/?input_json={{%22appids%22:[{AppId}],%22country_code%22:%22{CountryCode}%22,%22language%22:%22{Language}%22,%22query_app_title%22:true,%22query_package_price%22:true,%22query_app_description%22:true}}");
 
                 if (steamResponse == null || steamResponse.Response == null || steamResponse.Response.Apps == null || steamResponse.Response?.Apps?.Length == 0)
-                    return Json("response:{}");
-
-                if (steamResponse.Response?.Apps[0].OriginalPrice == null) // Song has been delisted, and we need to fake the data so the shop doesn't break.
-                {
-                    steamResponse.Response.Apps[0].OriginalPrice = 0;
-                    steamResponse.Response.Apps[0].CurrentPrice = 0;
-                    steamResponse.Response.Apps[0].PackageId = random.Next(10000);
-                    steamResponse.Response.Apps[0].DiscountPercent = 0;
-                    steamResponse.Response.Apps[0].Currency = CountryCodeToCurrency.ContainsKey(CountryCode) ? CountryCodeToCurrency[CountryCode] : "USD"; // Try to get the currency from our currency dictionary, else revert to USD.
-                    steamResponse.Response.Apps[0].Description = "DELISTED! Unable to purchase. " + steamResponse.Response.Apps[0].Description;
-                }
-                else
-                {
-                    if (!CountryCodeToCurrency.ContainsKey(CountryCode) && steamResponse.Response.Apps[0].Currency != null) // We haven't seen this currency. Lets store it so we can use it for delisted content.
-                        CountryCodeToCurrency.Add(CountryCode, steamResponse.Response.Apps[0].Currency);
-                }
+                    return GenericError;
 
                 Debug.WriteLine($"Sent response for {AppId} - {steamResponse.Response.Apps[0].Title}");
 
                 JsonResult result = Json(steamResponse);
+
+                if (steamResponse.Response?.Apps[0].OriginalPrice == null) // Song has been delisted, and we need to fake the data so the shop doesn't break.
+                {
+                    result = GenericError;
+                }
 
                 if (!Cache.ContainsKey(CountryCode)) // We haven't seen this country code before.
                 {
